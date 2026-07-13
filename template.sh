63179eb807c5ef306f4065086744922afd437503
Tariful Islam <tarifulsarkar009@gmail.com>
2026-07-14 01:16:51 +0600

    project template added

---
+++ template.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# 1. Set your project name
+export PROJECT_NAME="NexusView"
+
+# 2. Create the folder structure
+mkdir -p .github/workflows \
+         src/$PROJECT_NAME \
+         tests/unit \
+         tests/integration
+
+# 3. Create the files
+touch .github/workflows/.gitkeep \
+      src/$PROJECT_NAME/__init__.py \
+      tests/__init__.py \
+      tests/unit/__init__.py \
+      tests/integration/__init__.py \
+      requirements.txt \
+      requirements_dev.txt \
+      setup.py \
+      pyproject.toml \
+      setup.cfg \
+      tox.ini \
